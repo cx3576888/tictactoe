3405d66ee4cb29bd529c89bfea7c2691350786bf
cx3576888 <cx3576888@gmail.com>
2023-03-05 00:09:10 +0800

    tic tac toe codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,51 @@
-import React from 'react';
+import './App.css';
+import React, { useState } from 'react';
+import Board from './Board';
+import { SquareValue } from './Square';
+import calculateWinner from './calculateWinner';
 
-function App() {
+const App = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  const [winner, setWinner] = useState<SquareValue | null>(null);
+
+  const handleClick = (i: number) => {
+    if (winner || squares[i]) {
+      return;
+    }
+    const newSquares = squares.slice();
+    newSquares[i] = xIsNext ? 'X' : 'O';
+    setSquares(newSquares);
+    setXIsNext(!xIsNext);
+    const newWinner = calculateWinner(newSquares);
+    setWinner(newWinner);
+  };
+
+  const handleRestart = () => {
+    setSquares(Array(9).fill(null));
+    setXIsNext(true);
+    setWinner(null);
+  };
+
+  const renderStatus = () => {
+    if (winner) {
+      return `Winner: ${winner}`;
+    } else if (!winner && squares.every((square) => square !== null)) {
+      return 'Draw';
+    } else {
+      return `Next player: ${xIsNext ? 'X' : 'O'}`;
+    }
+  };
+
   return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
+    <>
+      <Board squares={squares} onClick={handleClick} />
+      <div className="game-info">
+        <div>{renderStatus()}</div>
+        <button className="game-button" onClick={handleRestart}>Restart</button>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/Square.tsx
@@ -0,0 +1,19 @@
+import './Square.css';
+import React from 'react';
+
+export type SquareValue = 'X' | 'O' | null;
+
+interface SquareProps {
+  value: string | null;
+  onClick: () => void;
+}
+
+const Square: React.FC<SquareProps> = ({ value, onClick }) => {
+  return (
+    <button className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
+};
+
+export default Square;
